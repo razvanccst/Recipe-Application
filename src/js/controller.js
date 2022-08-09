9d7cf62294c74891153132d49787766960b60549
Razvan <razvanccst@users.noreply.github.com>
2022-08-09 13:39:44 +0300

    Search and Pagination Implemented

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,60 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const controlRecipes = async function () {
+  try {
+    // Get the id as url hashID
+    const id = window.location.hash.slice(1);
+    // Guard Clause
+    if (!id) return;
+    //Rander Spinner
+    recipeView.renderSpinner();
+    // Loading Recipe
+
+    await model.loadRecipe(id);
+
+    // Randering Recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.error(err);
+    recipeView.renderError();
+  }
 };
 
-// https://forkify-api.herokuapp.com/v2
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    //Get search qurry
+    const query = searchView.getQuery();
+    if (!query) return;
 
-///////////////////////////////////////
+    //Load search results
+    await model.loadSearchResults(`${query}`);
+
+    //Rander results
+    resultsView.render(model.getSearchResultsPage());
+
+    //Rander initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  paginationView.render(model.state.search);
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+init();
